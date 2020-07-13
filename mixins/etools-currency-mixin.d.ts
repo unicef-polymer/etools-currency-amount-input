6a2d47f1360245a92ab39082559ab69c0400372e
laza88 <lazaryandrei@gmail.com>
2020-07-13 17:01:44 +0300

    currency mixin adjustments

--- mixins/etools-currency-mixin.d.ts
+++ mixins/etools-currency-mixin.d.ts
@@ -1,18 +1,20 @@
+import {PolymerElement} from '@polymer/polymer';
+import {LitElement} from 'lit-element';
 
-import { PolymerElement } from '@polymer/polymer';
 export {EtoolsCurrency};
 
 
 interface Constructor<T = {}> {
-  new (...args: any[]) : T;
- }
+    new(...args: any[]): T;
+}
+
 /**
-* Currency amount input. US format only.
-*/
-declare function EtoolsCurrency<T extends Constructor<PolymerElement>>(base: T):
-{
- new (...args: any[]): {
-   addCurrencyAmountDelimiter(value: any): any;
-   displayCurrencyAmount(value: any, placeholder?: any, noOfDecimals?: any): any;
- }
-} & T & Constructor<PolymerElement>;
+ * Currency amount input. US format only.
+ */
+declare function EtoolsCurrency<T extends Constructor<PolymerElement | LitElement>>(base: T):
+    {
+        new(...args: any[]): {
+            addCurrencyAmountDelimiter(value: any): any;
+            displayCurrencyAmount(value: any, placeholder?: any, noOfDecimals?: any): any;
+        }
+    } & T & Constructor<PolymerElement | LitElement>;
